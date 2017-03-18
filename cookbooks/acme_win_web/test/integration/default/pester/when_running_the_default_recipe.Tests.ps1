1ba4f2c1cc40d46546f325339324dd3c09be479f
Robb Schiefer <rschiefer@live.com>
2017-03-18 09:47:57 -0500

    converted double to single quotes

--- cookbooks/acme_win_web/test/integration/default/pester/when_running_the_default_recipe.Tests.ps1
+++ cookbooks/acme_win_web/test/integration/default/pester/when_running_the_default_recipe.Tests.ps1
@@ -1,19 +1,19 @@
-describe "when_running_the_default_recipe" {
+describe 'when_running_the_default_recipe' {
 
-  it "should create Data folder" {
+  it 'should create Data folder' {
       Test-Path 'C:\Data' | should be $true
   }
-  it "should create Log folder" {
+  it 'should create Log folder' {
       Test-Path 'C:\Logs' | should be $true
   }
   
   
-  it "should create Logs file share" {
-    $folderName = "Logs"
-    (& "net" "share" $folderName) | should not be $null
+  it 'should create Logs file share' {
+    $folderName = 'Logs'
+    (& 'net' 'share' $folderName) | should not be $null
 
-    it "should give all users read access to Logs file share" {
-        (& "net" "share" $folderName)[6].Contains("Everyone, READ") | should be $true
+    it 'should give all users read access to Logs file share' {
+        (& 'net' 'share' $folderName)[6].Contains('Everyone, READ') | should be $true
     }
   }
 }
